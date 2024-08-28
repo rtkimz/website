0537060bd2304e900ce9e0710d65e1d6b455918d
rtkimz <19673981+rtkimz@users.noreply.github.com>
2024-08-28 10:39:03 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
     title: 'Kids on Computers',
     tagline: 'Bringing technology to underprivileged kids worldwide',
-    url: 'https://kidsoncomputers.github.io/',
+    url: 'https://kidsoncomputers.github.io',
     baseUrl: '/website/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
